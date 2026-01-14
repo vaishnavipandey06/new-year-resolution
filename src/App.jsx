6f4c4914f127f782b82058dadbab3a12b7a37ffa
Syed-faiz05 <syedfaiz052004@gmail.com>
2026-01-14 22:23:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-slate-950">
+      <h1 className="text-6xl font-extrabold text-sky-400">
+        Tailwind is LIVE 🚀
+      </h1>
+    </div>
+  );
+}
+
+
+
